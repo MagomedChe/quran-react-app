f93ccb7d4fa005690ca83510fb056fcee4efa30e
MagomedChe <mdweb@bk.ru>
2023-04-19 14:39:07 +0300

    Выравнил марджины для мобильной версии

--- src/components/ListSuras/Sura.jsx
+++ src/components/ListSuras/Sura.jsx
@@ -8,11 +8,11 @@ function Sura({ sura }) {
   return (
     <>
       <NavLink
-        className='w-full md:w-w48 lg:w-130 mx-1 text-gray-400 font-medium h-28 flex  border-b border-gray-300 md:border md:rounded-xl md: mb-3 '
+        className='w-full md:w-w48 lg:w-130 mx-1 text-gray-400 font-medium h-28 flex border-b border-gray-300 md:border md:rounded-xl md:mb-3'
         to={`${sura.id}`}
       >
         <NumberSuras sura={sura} />
-        <div className='w-full lg:w-130 ml-8 mr-3  my-auto items-center'>
+        <div className='w-full lg:w-130 ml-5 mr-3  my-auto items-center'>
           <TotalAndTranslationSuras sura={sura} />
           <NameAndTransliterationSuras sura={sura} />
         </div>

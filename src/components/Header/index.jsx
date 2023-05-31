05460136218494469ce4042f66d6d0a82cb4df82
MagomedChe <mdweb@bk.ru>
2023-05-31 21:21:20 +0300

    проверка коммитов

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,16 +2,16 @@ import { NavLink } from "react-router-dom";
 
 function Header(props) {
   return (
-    <header className='w-full flex my-8 justify-between text-cyan-500 text-6xl'>
+    <header className="w-full flex my-8 justify-between text-cyan-500 text-6xl">
       <NavLink to={"/"}>
-        <div className=''>القرآن</div>
+        <div className="casdf">القرآن</div>
       </NavLink>
-      <div className='flex'>
-        <div className='ml-8'>
-          <span className='material-symbols-outlined text-4xl'>search</span>
+      <div className="flex">
+        <div className="ml-8">
+          <span className="material-symbols-outlined text-4xl">search</span>
         </div>
-        <div className='ml-8'>
-          <span className='material-symbols-outlined text-4xl'>headphones</span>
+        <div className="ml-8">
+          <span className="material-symbols-outlined text-4xl">headphones</span>
         </div>
       </div>
     </header>

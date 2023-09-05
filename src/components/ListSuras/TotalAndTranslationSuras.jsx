4daf66c1994a091b161b879bdd852e00c374a99a
MagomedChe <mdweb@bk.ru>
2023-09-05 09:21:15 +0300

    доработка верстки страницы аятов

--- src/components/ListSuras/TotalAndTranslationSuras.jsx
+++ src/components/ListSuras/TotalAndTranslationSuras.jsx
@@ -2,7 +2,11 @@ import React from "react";
 const TotalAndTranslationSuras = React.forwardRef(({ sura }, ref) => {
   return (
     <div ref={ref} className="flex justify-between items-center mb-2 ">
-      <div>{sura.total_verses} аятов</div>
+      <div>
+        {sura.total_verses <= 4
+          ? `${sura.total_verses} аята`
+          : `${sura.total_verses} аятов`}
+      </div>
       <div>{sura.translation}</div>
     </div>
   );
--- src/components/OneSura/index.jsx
+++ src/components/OneSura/index.jsx
@@ -5,34 +5,39 @@ function OneSura(props) {
   const id = parseInt(useParams().id);
   const { data = [], isLoading } = useGetOneSuraQuery(id);
   if (isLoading) return <h1>Loading sura ...</h1>;
-  console.log(id);
+  console.log(data);
   return (
     <>
-      <div>
-        <div>{data.name}</div>
-        <div>Сура "{data.translation}"</div>
+      <div className="flex justify-between py-9">
+        <div className="text-xl">
+          {data.total_verses <= 4
+            ? `${data.total_verses} аята`
+            : `${data.total_verses} аятов`}
+        </div>
+        <div className="text-cyan-500 text-3xl">{data.name}</div>
+        <div className="text-xl">Сура "{data.translation}"</div>
       </div>
       <div className="break-keep grid justify-items-end w-full flex-wrap flex-row-reverse">
         {data.verses.map((oneSura) => {
           return (
             <div
               key={oneSura.id}
-              className="my-7 relative text-3xl flex items-center break-keep"
+              className="my-7 pb-7 relative text-3xl inline-flex items-center break-keep w-full border-b-2"
             >
-              <p
+              <span
                 dir="rtl"
-                className="relative break-all text-3xl flex items-center"
+                className="relative text-3xl inline items-center leading-loose  w-full"
               >
                 {oneSura.text}
-                <div className="relative text-2xl flex items-center">
+                <div className="relative text-2xl inline-flex items-center">
                   <span className=" material-symbols-outlined text-7xl text-center absolute">
                     brightness_empty
                   </span>
-                  <span className=" text-lg text-center w-18">
+                  <span className="inline text-lg text-center w-18">
                     {oneSura.id}
                   </span>
                 </div>
-              </p>
+              </span>
             </div>
           );
         })}

ced70d4b187255325f814a4e1d38fc0909500c0b
MagomedChe <mdweb@bk.ru>
2023-09-06 16:00:50 +0300

    Добавлено выдвигающееся меню

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,11 @@
 import { NavLink } from "react-router-dom";
+import SettingsIcon from "@mui/icons-material/Settings";
+import IconButton from "@mui/material/IconButton";
+import HiddenMenu from "./HiddenMenu";
+import { useState } from "react";
 
 function Header(props) {
+  const [isOpenMenu, setOpenMenu] = useState(false);
   return (
     <header className="w-full flex my-8 justify-between text-cyan-500 text-6xl">
       <NavLink to={"/"}>
@@ -12,6 +17,22 @@ function Header(props) {
         </div>
         <div className="ml-8">
           <span className="material-symbols-outlined text-4xl">headphones</span>
+        </div>
+        <div>
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <SettingsIcon />
+          </IconButton>
+
+          <HiddenMenu
+            openMenu={isOpenMenu}
+            closeMenu={() => setOpenMenu(false)}
+          />
         </div>
       </div>
     </header>
